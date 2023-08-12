8ab254022e20e1d46a4504bad6b7be0ae14890b0
Ian D'Souza <dsouza.ianp@gmail.com>
2023-08-12 14:10:41 -0400

    added image, working on styling

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,8 +1,38 @@
+"use client"
 import React from 'react'
-
+import Image from "next/image"
+import { Link } from 'react-scroll/modules'
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <section id="home">
+      <div className="flex">
+        <div>
+            <Image src ="/headshot.png" alt="" width = {300} height={300}/>
+        </div>
+        <div>
+            <h1>Hi, I&#39;m Ian!</h1>
+            <p>
+              I&#39;m a{" "}
+              <span className="font-semibold text-teal-600">
+                Student / Software Engineer{" "} 
+              </span>
+              from Shrewsbury, MA. I&#39;m currently a junior at the University of Massachusetts Amherst,
+              working towards my B.S. in Computer Science.
+            </p>
+            <Link
+            to = "projects"
+            className = "text-neutral-100 font-semibold px-6 py-3 bg-teal-600"
+            activeClass = "active"
+            spy={true}
+            smooth = {true}
+            offset={-100}
+            duration={500}
+            >
+              Projects
+            </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
